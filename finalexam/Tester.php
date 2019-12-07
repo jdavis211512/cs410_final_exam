a1512cb98889ae4bc806a1138f5de1dac8aae96a
jdavis211512 <Jwdavis2@bsu.edu>
2019-12-07 17:21:23 -0500

    needs testing

--- finalexam/Tester.php
+++ finalexam/Tester.php
@@ -2,13 +2,11 @@
 include_once 'db.php';
 $query = "insert into Bugs (EnteredBy, Subject, Priority, Description, AssignedTo, Status, Changes) VALUES (:EnteredBy,:Subject,:priority,:Description,Null, :Status,0 )";
 $sql=$conn->prepare($query);
-$sql->bindValue(':EnteredBy',$_SESSION['userid']);
+$sql->bindValue(':EnteredBy',$_SESSION['name']);
 $sql->bindValue(':Subject',$_POST['Subject']);
 $sql->bindValue(':priority',$_POST['priority']);
 $sql->bindValue(':Description',$_POST['description']);
 $sql->execute();
-
-
 ?>
 <html
 ><head>
